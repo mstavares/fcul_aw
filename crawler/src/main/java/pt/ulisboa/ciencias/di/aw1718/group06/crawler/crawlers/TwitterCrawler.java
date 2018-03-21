4ab993f655872df1d79add8a5ec6a75fd3914540
Malgorzata Salawa <mksalawa@gmail.com>
2018-03-21 13:19:19 +0000

    Init twitter crawler, with no DB.

--- crawler/src/main/java/pt/ulisboa/ciencias/di/aw1718/group06/crawler/crawlers/TwitterCrawler.java
+++ crawler/src/main/java/pt/ulisboa/ciencias/di/aw1718/group06/crawler/crawlers/TwitterCrawler.java
@@ -1,21 +1,43 @@
 package pt.ulisboa.ciencias.di.aw1718.group06.crawler.crawlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pt.ulisboa.ciencias.di.aw1718.group06.dataaccess.Disease;
 import pt.ulisboa.ciencias.di.aw1718.group06.dataaccess.DiseaseCatalog;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 public class TwitterCrawler extends Crawler {
-	
+    private static final Logger logger = LoggerFactory.getLogger(TwitterCrawler.class);
+
 	private final String BASE_URL = "";
-	
-	public TwitterCrawler(DiseaseCatalog diseaseCatalog) {
+    private final Twitter twitter;
+    private int STORED_TWEET_COUNT = 10;
+
+    public TwitterCrawler(DiseaseCatalog diseaseCatalog, Twitter twitter) {
 		super(diseaseCatalog);
+        this.twitter = twitter;
 	}
 
 
 	@Override
 	public boolean update(Disease disease) {
-		// TODO Auto-generated method stub
-		return false;
+
+        Query query = new Query(disease.getName());
+        query.setCount(STORED_TWEET_COUNT);
+        try {
+            QueryResult result = this.twitter.search(query);
+            for (Status status : result.getTweets()) {
+                logger.info("@" + status.getUser().getScreenName() + ":" + status.getText());
+            }
+        } catch (TwitterException e) {
+            logger.error("Error while looking up results for \"" + disease.getName() + "\"", e);
+            return false;
+        }
+		return true;
 	}
 
 }
--- crawler/src/main/java/pt/ulisboa/ciencias/di/aw1718/group06/crawler/startup/Main.java
+++ crawler/src/main/java/pt/ulisboa/ciencias/di/aw1718/group06/crawler/startup/Main.java
@@ -3,11 +3,17 @@ package pt.ulisboa.ciencias.di.aw1718.group06.crawler.startup;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pt.ulisboa.ciencias.di.aw1718.group06.crawler.crawlers.TwitterCrawler;
+import pt.ulisboa.ciencias.di.aw1718.group06.dataaccess.Disease;
+import pt.ulisboa.ciencias.di.aw1718.group06.dataaccess.DiseaseCatalog;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -40,19 +46,49 @@ public class Main {
             return;
         }
 
-        try(Connection conn = dataSource.getConnection()) {
-            Statement stmt = conn.createStatement();
-            ResultSet result = stmt.executeQuery("SELECT * FROM diseases");
+        DiseaseCatalog catalog = DiseaseCatalog.getInstance();
 
-            while (result.next()) {
-                int id = result.getInt("id");
-                String name = result.getString("name");
-                String description = result.getString("abstract");
-                String derivedFrom = result.getString("was_derived_from");
-                logger.info("{}: {} (from: {}) - {}", id, name, derivedFrom, description);
+        try(Connection conn = dataSource.getConnection()) {
+
+            String SQL_INSERT = "INSERT INTO diseases (name, abstract, was_derived_from) VALUES (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+            String diseaseName = "asthma";
+            String diseaseDescription = "asthma description";
+            String derivedFrom = "derived from";
+            statement.setString(1, diseaseName);
+            statement.setString(2, diseaseDescription);
+            statement.setString(3, derivedFrom);
+
+            int affected = statement.executeUpdate();
+            if (affected == 0) {
+                throw new SQLException("Creating entry failed, no rows affected.");
             }
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
+                if (keys.next()) {
+                    int id = keys.getInt(1);
+                    Disease disease = new Disease(id, diseaseName, diseaseDescription, derivedFrom);
+
+                    Twitter twitter = TwitterFactory.getSingleton();
+                    TwitterCrawler twitterCrawler = new TwitterCrawler(catalog, twitter);
+                    twitterCrawler.update(disease);
+                }
+            }
+
+            Statement stmt = conn.createStatement();
+            try (ResultSet result = stmt.executeQuery("SELECT * FROM diseases")) {
+                while (result.next()) {
+                    int id = result.getInt("id");
+                    String name = result.getString("name");
+                    String description = result.getString("abstract");
+                    String derived = result.getString("was_derived_from");
+                    logger.info("{}: {} (from: {}) - {}", id, name, derived, description);
+                }
+            }
+
         } catch (SQLException e) {
             logger.error("Error while connecting to database: ", e);
         }
-	}
+
+    }
 }
