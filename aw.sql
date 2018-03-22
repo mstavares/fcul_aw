fb4a008a2ad777f79958625103458c26c3b26635
Tirieru <tiriel@outlook.com>
2018-03-22 23:11:33 +0000

    sql changes and db connection settings

--- aw.sql
+++ aw.sql
@@ -3,29 +3,29 @@ USE wa;
 
 CREATE TABLE diseases (
 	id INT NOT NULL AUTO_INCREMENT, 
-	name VARCHAR(255) NOT NULL, 
-	abstract VARCHAR(255), 
+	name VARCHAR(255) UNIQUE NOT NULL, 
+	abstract MEDIUMTEXT, 
 	was_derived_from VARCHAR(255), 
 	PRIMARY KEY (id)
 )ENGINE=InnoDB; 
 
 CREATE TABLE images (
 	id INT NOT NULL AUTO_INCREMENT,
-	url VARCHAR(255) NOT NULL,
+	url VARCHAR(255) UNIQUE NOT NULL,
 	PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
 CREATE TABLE tweets (
 	id INT NOT NULL AUTO_INCREMENT,
-	url VARCHAR(255) NOT NULL,
+	url VARCHAR(255) UNIQUE NOT NULL,
 	text VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
 CREATE TABLE pubmed (
 	id INT NOT NULL AUTO_INCREMENT,
-	title VARCHAR(255) NOT NULL,
-	abstract VARCHAR(255),
+	title VARCHAR(255) UNIQUE NOT NULL,
+	abstract MEDIUMTEXT,
 	PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
@@ -34,8 +34,8 @@ CREATE TABLE diseases_images (
 	id_images INT NOT NULL,
 	black_listed BOOLEAN DEFAULT false,
 	PRIMARY KEY (id_diseases, id_images),
-	FOREIGN KEY (id_diseases) REFERENCES diseases(id) ON UPDATE CASCADE,
-	FOREIGN KEY (id_images) REFERENCES images(id) ON UPDATE CASCADE
+	FOREIGN KEY (id_diseases) REFERENCES diseases(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_images) REFERENCES images(id) ON UPDATE CASCADE ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 CREATE TABLE diseases_tweets (
@@ -43,17 +43,17 @@ CREATE TABLE diseases_tweets (
 	id_tweets INT NOT NULL,
 	black_listed BOOLEAN DEFAULT false,
 	PRIMARY KEY (id_diseases, id_tweets),
-	FOREIGN KEY (id_diseases) REFERENCES diseases(id) ON UPDATE CASCADE,
-	FOREIGN KEY (id_tweets) REFERENCES tweets(id) ON UPDATE CASCADE
-);
+	FOREIGN KEY (id_diseases) REFERENCES diseases(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_tweets) REFERENCES tweets(id) ON UPDATE CASCADE ON DELETE CASCADE
+)ENGINE=InnoDB;
 
 CREATE TABLE diseases_pubmed (
 	id_diseases INT NOT NULL,
 	id_pubmed INT NOT NULL,
 	PRIMARY KEY (id_diseases, id_pubmed),
-	FOREIGN KEY (id_diseases) REFERENCES diseases(id) ON UPDATE CASCADE,
-	FOREIGN KEY (id_pubmed) REFERENCES pubmed(id) ON UPDATE CASCADE
-);
+	FOREIGN KEY (id_diseases) REFERENCES diseases(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_pubmed) REFERENCES pubmed(id) ON UPDATE CASCADE ON DELETE CASCADE
+)ENGINE=InnoDB;
 
 
 
