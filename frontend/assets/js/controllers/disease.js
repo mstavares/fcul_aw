753e24f6eefbaf5bf2c818b8d2f7447d0c8a59ac
Tirieru <tiriel@outlook.com>
2018-05-18 13:55:11 +0100

    more pubmed and css fixes

--- frontend/assets/js/controllers/disease.js
+++ frontend/assets/js/controllers/disease.js
@@ -32,6 +32,7 @@ if (diseaseId != undefined){
 
 			document.getElementById('disease-name').innerHTML = "Disease: " + content.name;
 
+			var diseaseId = content.id;
 			var info = "";
 			var articles = "";
 			var tweets = "";
@@ -44,10 +45,19 @@ if (diseaseId != undefined){
 			if(content.dead !== null)
 				info += "<b>People killed by this disease:</b> " + content.dead + "<br />";
 
+			info += "<b>Related diseases:</b> ";
+
+			for (var i = 0; i < content.diseases.length; i++){
+				info += "<a href='#disease=" + content.diseases[i].id + "'>" + content.diseases[i].name + "</a>, ";
+			}
+
 			for (var i = 0; i < content.articles.length; i++){
+				var pubmedId = content.articles[i].id;
+				var args = diseaseId + ", " + pubmedId + ", 4";
+				var argsImplicit = diseaseId + ", " + pubmedId + ", 0";
 				articles += "<div class='pubmed-container'>"
-					articles += "<div class='pubmed-feedback' onclick='updatePubMedFeedback()'></div>"
-					articles += "<div class='pubmed-title'><a href='https://www.ncbi.nlm.nih.gov/pubmed/" + content.articles[i].pubMedId + "'>" + content.articles[i].title + "</a></div>"
+					articles += "<div class='pubmed-feedback' onclick='updatePubMedFeedback(" + args + ")'>not relevant</div>"
+					articles += "<div class='pubmed-title'><a target='blank' href='https://www.ncbi.nlm.nih.gov/pubmed/" + content.articles[i].pubMedId + "' onclick=updatePubMedFeedback(" + argsImplicit + ")'>" + content.articles[i].title + "</a></div>"
 					articles += "<div class='pubmed-description'>" + content.articles[i].description + "</div>"
 					articles += "<div class='pubmed-related'><b>Related diseases:</b> "; 
 					for (var j = 0; j < content.articles[i].mentionedDiseases.length; j++){
@@ -83,4 +93,59 @@ if (diseaseId != undefined){
     ajax.open('GET', apiBaseUrl + "disease/get/" + diseaseId, true);
 	ajax.send();
 	
+}
+
+
+function updatePubMedFeedback(diseaseId, pubmedId, op){
+	ajax.onreadystatechange = function () {
+
+        var html = ajax.responseText;
+        if (ajax.readyState == 4 && ajax.status == 200) {
+
+			var content = JSON.parse(html);
+
+			console.log("Feedback PubMed: " + content);
+		}
+
+    };
+    
+    ajax.open('POST', apiBaseUrl + "feedback/pubmed?diseaseId=" + diseaseId + "&pubmedId=" + pubmedId + "&operation=" + op, true);
+	ajax.send();
+}
+
+
+
+function updateImgFeedback(diseaseId, imgId, op){
+	ajax.onreadystatechange = function () {
+
+        var html = ajax.responseText;
+        if (ajax.readyState == 4 && ajax.status == 200) {
+
+			var content = JSON.parse(html);
+
+			console.log("Feedback Image: " + content);
+		}
+
+    };
+    
+    ajax.open('POST', apiBaseUrl + "feedback/image?diseaseId=" + diseaseId + "&imageId=" + imageId + "&operation=" + op, true);
+	ajax.send();
+}
+
+
+function updateTweetFeedback(diseaseId, tweetId, op){
+	ajax.onreadystatechange = function () {
+
+        var html = ajax.responseText;
+        if (ajax.readyState == 4 && ajax.status == 200) {
+
+			var content = JSON.parse(html);
+
+			console.log("Feedback Tweet: " + content);
+		}
+
+    };
+    
+    ajax.open('POST', apiBaseUrl + "feedback/tweet?diseaseId=" + diseaseId + "&tweetId=" + tweetId + "&operation=" + op, true);
+	ajax.send();
 }
